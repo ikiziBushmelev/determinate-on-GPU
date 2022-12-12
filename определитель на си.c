c2d8365b9100c657c786712f0d9df7eafe7671ea
ikiziBushmelev <artembushmelevgit@gmail.com>
2022-12-12 20:31:17 +0300

    Update определитель на си.c
    Были сделаны отдельные функции ввода и вывода. Написана функция для приведения матрицы к треугольной.

--- определитель на си.c
+++ определитель на си.c
@@ -4,14 +4,51 @@
 
 
 
+void determinant(double** matrix, int N)
+{
+    for (int k = 0; k < N; k++)
+    {
+        for (int i = k + 1; i < N; i++)
+        {
+            double mu = matrix[i][k] / matrix[k][k];
+            for (int j = 0; j < N; j++)
+                matrix[i][j] -= matrix[k][j] * mu;
+        }
+    }
+}
+void filling_the_matrix(double** matrix,int N)
+{
+    srand(time(NULL));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrix[i][j] = 1 + rand() % 9;
+        }
+    }
+}
 
+void matrix_output(double** matrix, int N)
+{
+    printf("\n");
+    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%0.2f ", matrix[i][j]);
+            if (j==N-1) printf("\n");
+        }
+    }
+    printf("\n");
+}
 int main()
 {
     int N;
     printf("Input size of matrix N = ");
     scanf("%i", &N);
 
-    int time_start = clock();
+    
 
     double** input_matrix = NULL;
     input_matrix = (double**)malloc(N * sizeof(double*));
@@ -19,27 +56,11 @@ int main()
     {
         input_matrix[i] = (double*)malloc(N * sizeof(double));
     }
-
-    
-    srand(time(NULL));
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            input_matrix[i][j] = 1 + rand() % 9;
-        }
-    }
-
-    printf("\n");
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            printf("%0.2f ", input_matrix[i][j]);
-            if (((j + 1) % N == 0) && (j != 0)) printf("\n");
-        }
-    }
-    printf("\n");
+    filling_the_matrix(input_matrix,N);
+    matrix_output(input_matrix,N);
+    int time_start = clock();
+    determinant(input_matrix, N);
+    matrix_output(input_matrix,N);
 
    
     
