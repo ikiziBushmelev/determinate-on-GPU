31047315fc43c7766f81e20d07a5c4f14cc7c18b
GusarovV223 <gusarovvladimir7111@gmail.com>
2022-12-13 11:51:29 +0300

    Update asyncAPI.cu
    добавлено приведение матрицы к треугольному виду на GPU и вывод ее в консоль

--- asyncAPI.cu
+++ asyncAPI.cu
@@ -1,16 +1,50 @@
+#ifndef CUDACC
+#define CUDACC
+#endif
+#include <cuda.h>
+#include <device_functions.h>
+#include <cuda_runtime_api.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <cuda.h>
 #include <curand_kernel.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <random>
 #include <conio.h>
+
+
+//blockDim - размер блока
+//blockIdx - индекс текущего блока
+//threadIdx - индекс текущей нити в блоке
+
 
 __global__ void test(double* arr, int N)
 {
-    double x = 10;
-    arr[N] = x+5;
-    arr[0] = x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j;
+    double kof;
+    for (j = 0; j < N; j++)
+    {
+        if (i >= j && i < N - 1)
+        {
+            kof = arr[(i + 1) * N + j] / arr[j * N + j];
+            int g = blockIdx.y * blockDim.y + threadIdx.y;
+            //printf("g = %d i = %d j = %d  kof = %.6f\n", g, i, j, kof);
+            //__syncthreads();
+
+            if (g < N)
+            {
+                arr[(i + 1) * N + g] -= kof * arr[j * N + g];
+                //printf("g posle = %d\n", g);
+                printf(".");
+            }
+
+        }
+        // __syncthreads();
+    }
+    // arr[0] = 10;
 }
 
 __host__ int main()
@@ -28,6 +62,15 @@ __host__ int main()
     {
         pMatr[i] = 1 + rand() % 9;
     }
+    pMatr[0] = 8;
+    pMatr[1] = 6;
+    pMatr[2] = 6;
+    pMatr[3] = 8;
+    pMatr[4] = 1;
+    pMatr[5] = 9;
+    pMatr[6] = 8;
+    pMatr[7] = 5;
+    pMatr[8] = 3;
     printf("\n");
     for (int i = 0; i < SizeMatr; i++)
     {
@@ -45,18 +88,18 @@ __host__ int main()
     dim3 gridSize = dim3(N, N, 1);//Размерность сетки блоков (dim3), выделенную для расчетов
     dim3 blockSize = dim3(1, 1, 1);//Размер блока (dim3), выделенного для расчетов
 
-    
-    test << < gridSize, blockSize >> > (pMatr_GPU, N); // тестовый вызов функции для изменения матрицы 
+
+    test << < gridSize, blockSize >> > (pMatr_GPU, N); // вызов функции для изменения матрицы 
 
     cudaThreadSynchronize();//Синхронизируем потоки
     cudaMemcpy(pMatr, pMatr_GPU, SizeInByte, cudaMemcpyDeviceToHost);//Копируем новую матрицу с GPU обратно на CPU
-
+    printf("\n");
     for (int i = 0; i < SizeMatr; i++)  //выводим измененную матрицу
     {
-        printf("%0.2f ", pMatr[i]); 
+        printf("%0.2f ", pMatr[i]);
         if (((i + 1) % N == 0) && (i != 0)) printf("\n");
     }
     printf("\n");
-   
+
     return 0;
 }
